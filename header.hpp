b40b719c74e55c612f3e0f8804c21566580708b9
Kyle Nguyen <knguyen1193@outlook.com>
2024-03-03 23:22:39 -0800

    add some code base

---
+++ header.hpp
@@ -0,0 +1,14 @@
+#ifndef DOIT_CPP
+#define DOIT_CPP
+#include "iostream"
+#include "fstream" 
+#include "vector"
+#include "chrono"
+#include <limits>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+#endif // !DOIT_CPP
